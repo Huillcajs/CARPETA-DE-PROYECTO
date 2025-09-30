e8357cbd2413d1e91cdaea7c3f6a5bcfdc896877
bymichelleah <148379625+bymichelleah@users.noreply.github.com>
2025-09-29 19:35:26 -0500

    Agregando rutas de Dami

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,14 @@ use App\Http\Controllers\Auth\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/actividad', function(){
+    return view('actividades.actividad');
+});
+
+Route::get('/guia', function(){
+    return view('guias_practicas.guia');
+});
+
+Route::get('/descargar-actividad/{nombre}', [ACTIVIDADController::class, 'descargarActividad'])->name('descargar.actividad');
+Route::get('/descargar-guia/{nombre}', [GUIASController::class, 'descargarGuia'])->name('descargar.guia');
